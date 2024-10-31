c15b8d3207fc4fbb74f790194cf5a57dbf72bae7
juan neira <juanneira43@gmail.com>
2024-10-31 13:30:03 -0300

    Cindicion igualdad

---
+++ ejercicio08.py
@@ -0,0 +1,7 @@
+#Condicion 02
+num = int(input("Ingresa un numero entero: "))
+if num%3 == 0 and num%5 == 0:
+    print(f"{num} es divisible por 3 y por 5")
+else:
+    print(f"{num} no es divisible por 3 y por 5") 
+       
