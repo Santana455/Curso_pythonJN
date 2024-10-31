afe522fcf962dffe80d7ece7d3e84d49441faa01
juan neira <juanneira43@gmail.com>
2024-10-30 22:43:14 -0300

    Operaciones Aritmetricas

--- ejercicio04.py
+++ ejercicio04.py
@@ -1,4 +1,5 @@
 #Este ejercicio es para Santana
+"""
 mensaje1 = input("Por favor ingresa tu nombre")
 mensaje2 = input("Por favor ingresa tu Apellido")
 print("El nombre es: ",mensaje1)
@@ -24,12 +25,25 @@ print("El valor de pi es: ",pi)
 es_verdadero = True
 es_falso = False
 print("Es verdadero: ",es_verdadero)
-print("Es falso: ",es_falso)
+print("Es falso: ",es_falso)"""
 
 #Operadores Aritmetricos
-numero1 = int(input("ingresa el numero 1: "))
-numero2 = int(input("Ingresa el numero 2: "))
+
+#Multiplicacion
+numero1 = float(input("ingresa el numero 1: "))
+numero2 = float(input("Ingresa el numero 2: "))
 multiplicacion = numero1*numero2
 print(multiplicacion)
 
+#Divicion
+division = numero1/numero2
+print(division)
+
+#Residuo o Modulo
+modulo = numero1%numero2
+print(modulo)
+
+#Division entera
+division_entera = numero1//numero2
+print(division_entera)
 
