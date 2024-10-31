8483f2586495220a197cf77efe1ba2e87c08dc53
juan neira <juanneira43@gmail.com>
2024-10-31 13:16:25 -0300

    Condicion 01

---
+++ ejercicio07.py
@@ -0,0 +1,13 @@
+#Condiciones 1
+num = int(input("Ingrese un numero entero: "))
+if num>0:
+    print(f"{num} es positivo")
+elif num<0:
+    print(f"{num} es negativo")
+else:
+    print("El numero es 0")
+    
+
+    
+    
+    
