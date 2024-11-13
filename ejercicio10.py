0176f951245a9e9df7bf6c9f8f06114caf3bdeb1
Santana455 <juanneira43@gmail.com>
2024-11-12 22:23:12 -0300

    Realizar for i in range

---
+++ ejercicio10.py
@@ -0,0 +1,4 @@
+cuadernos = int(input("Cuantos cuadernos tienes: "))
+for i in range(cuadernos):
+    print("El cuadernos es: "+str(i+1)+"cuadernos")
+    
---
+++ ejercicio11.py
@@ -0,0 +1,7 @@
+altura = int(input("Ingresa la altura del triangulo"))
+for i in range(altura):
+    for i in range(i+1):
+        print("*", end = "")
+    print("")
+        
+        
