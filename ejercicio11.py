a01f7629af96e7868381909ba8e7abe35117f1c1
Santana455 <juanneira43@gmail.com>
2024-11-13 21:00:36 -0300

    Realizar n

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,7 +1,7 @@
-altura = int(input("Ingresa la altura del triangulo"))
+altura = int(input("Ingrese la altura del triangulo: "))
 for i in range(altura):
     for i in range(i+1):
         print("*", end = "")
     print("")
-        
+    
         
---
+++ ejercicio12.py
@@ -0,0 +1,4 @@
+n = int(input("Ingresa el numero: "))
+for i in range(n, -1, -2):
+    print(i, end =  ",")
+    
