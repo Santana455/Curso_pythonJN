3179af647737b5e4826e25ab76953343b223aa8e
Santana455 <juanneira43@gmail.com>
2024-11-13 21:40:29 -0300

    Realizar While

---
+++ ejercicio13.py
@@ -0,0 +1,6 @@
+suma = 0
+numero = int(input("Introduce un numero (0 para termninar): "))
+while numero!=0:
+    suma+=numero
+    numero = int(input("Introduce otro numero (0 para termninar): "))
+print("El total es: ",suma) 
