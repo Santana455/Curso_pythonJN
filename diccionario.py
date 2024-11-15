3dd6860fe8d08f6a8a09474fc033338e81a9647b
Santana455 <juanneira43@gmail.com>
2024-11-15 20:41:12 -0300

    Realizar Diccionario

---
+++ diccionario.py
@@ -0,0 +1,18 @@
+estudiante = {
+    "nombre":"Juan",
+    "edad":"21",
+    "carrera":"Ingenieria de sistemas",
+    "promedio":"8.5"
+}
+print(estudiante["nombre"])
+estudiante["email"]="juanneira43@gmail.com"
+print(estudiante)
+
+claves = estudiante.keys()
+print(claves)
+
+valores = estudiante.values()
+print(valores)
+
+items = estudiante.items()
+print(items)
